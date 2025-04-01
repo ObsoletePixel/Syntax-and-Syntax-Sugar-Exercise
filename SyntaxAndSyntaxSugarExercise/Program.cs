59d2fc7c9b9ac81c858d566ca219db55794d20c1
ObsoletePixel <obsoletepixel@gmail.com>
2025-04-01 00:12:43 -0700

    first commit of syntax sugar projct

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var i = 4;
+            var response = (i>9) ? $"{i} is larger than 9" : $"{i} is smaller than 9";
+            Console.WriteLine(response);
         }
     }
 }
